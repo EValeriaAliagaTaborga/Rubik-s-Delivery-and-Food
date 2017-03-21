c6771ecdd9c4d2c2d6a08d70041244912e09ecb9
EValeriaAliagaTaborga <valealiaga2121@gmail.com>
2017-03-21 11:32:21 -0400

    Slide con ViewPager

--- app/src/main/java/notanamelessentreprise/rubiksdeliveryandfood/MenuActivity.java
+++ app/src/main/java/notanamelessentreprise/rubiksdeliveryandfood/MenuActivity.java
@@ -3,11 +3,16 @@ package notanamelessentreprise.rubiksdeliveryandfood;
 import android.content.Context;
 import android.content.Intent;
 import android.media.Image;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
@@ -23,6 +28,8 @@ public class MenuActivity extends AppCompatActivity {
     private ImageView imgHotDog;
     private ImageView imgPollo;
     private ImageView imgExtras;
+    ViewPager viewPager;
+
 
     private Context context;
 
@@ -34,6 +41,7 @@ public class MenuActivity extends AppCompatActivity {
 
         context = this;
 
+        viewPager = (ViewPager)findViewById(R.id.viewPager);
         lyhHamburguesa =(LinearLayout) findViewById(R.id.lyhHamburguesa);
         lyhPizza =(LinearLayout) findViewById(R.id.lyhPizza);
         lyhHotDog =(LinearLayout) findViewById(R.id.lyhHotDog);
@@ -45,14 +53,22 @@ public class MenuActivity extends AppCompatActivity {
         imgPollo = (ImageView) findViewById(R.id.imgPollo);
         imgExtras = (ImageView) findViewById(R.id.imgExtras);
 
-        Picasso.with(context).load("http://i1278.photobucket.com/albums/y518/TaurineSGH/Programacion%20III" +
-                "/btnham_zpss2i2xyad.png").placeholder(R.drawable.rubik2).into(imgHamburguesa);
-        Picasso.with(context).load("http://i1278.photobucket.com/albums/y518/TaurineSGH/Programacion%20III" +
-                "/btnpizza_zpsshpdbgwm.png").placeholder(R.drawable.rubik2).into(imgPizza);
-        Picasso.with(context).load("").placeholder(R.drawable.rubik2).into(imgHotDog);
-        Picasso.with(context).load("").placeholder(R.drawable.rubik2).into(imgPollo);
-        Picasso.with(context).load("http://i1278.photobucket.com/albums/y518/TaurineSGH/Programacion%20III" +
-                "/btnextras_zpseca2wzwu.png").placeholder(R.drawable.rubik2).into(imgExtras);
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(this);
+
+        viewPager.setAdapter(viewPagerAdapter);
+
+        //Picasso.with(context).load("http://i1278.photobucket.com/albums/y518/TaurineSGH/Programacion%20III" +
+        //        "/btnham_zpss2i2xyad.png").placeholder(R.drawable.rubik2).resize(100,100).into(imgHamburguesa);
+        //Picasso.with(context).load("http://i1278.photobucket.com/albums/y518/TaurineSGH/Programacion%20III" +
+        //        "/btnpizza_zpsshpdbgwm.png").placeholder(R.drawable.rubik2).resize(100,100).into(imgPizza);
+        //Picasso.with(context).load("http://i1278.photobucket.com/albums/y518/TaurineSGH/Programacion%20III" +
+        //        "/btnhotdog_zpsk1mbupua.png").placeholder(R.drawable.rubik2).resize(100,100).into(imgHotDog);
+        //Picasso.with(context).load("http://i1278.photobucket.com/albums/y518/TaurineSGH/Programacion%20III" +
+        //        "/btnpollo_zpskviqlidb.png").placeholder(R.drawable.rubik2).resize(100,100).into(imgPollo);
+        //Picasso.with(context).load("http://i1278.photobucket.com/albums/y518/TaurineSGH/Programacion%20III" +
+        //        "/btnextras_zpseca2wzwu.png").placeholder(R.drawable.rubik2).resize(100,100).into(imgExtras);
+
+
 
         lyhHamburguesa.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -93,5 +109,16 @@ public class MenuActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        viewPager.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v)
+            {
+                Toast.makeText(getApplicationContext(), "Notificación Toast", Toast.LENGTH_LONG);
+            }
+        });
+
+
     }
+
+
 }
